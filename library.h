f71e10012cf6bbe2d78cc171c0354aabf8138565
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2025-10-31 20:44:07 +0100

    now the game logic is done successfully

--- library.h
+++ library.h
@@ -3,6 +3,7 @@
 
 
 #define LEN_SQUARES 3
+#define DIDNT_HIT_3 33
 
 typedef struct s_data
 {
@@ -15,6 +16,8 @@ typedef struct s_info
     t_data square[3][3];
     int select_position_x;
     int select_position_y;
+    char turn;
+    int reached_3_hits;
 }   t_info;
 
 
--- main.c
+++ main.c
@@ -47,7 +47,7 @@ int init_info(t_info *info)
 {
     t_data *tmp;
 
-    // now we will considere everything as an embpty box and set everything to space
+    // now we will considere everything as an empty box and set everything to space
     int i = 0;
     while (i < 3)
     {
@@ -61,6 +61,8 @@ int init_info(t_info *info)
 
         i++;
     }
+    info->turn = 'X';
+    info->reached_3_hits = 0;
     init_select(info);
     return 0;
 }
@@ -68,66 +70,103 @@ int init_info(t_info *info)
 int move_select(t_info *info, int x, int y)
 {
     if (info->select_position_x + x > 2 || info->select_position_x + x < 0)
-        return (printf ("those are the limits, you cant skip pass the boarders !\n"), 1);
+        return (1);
     if (info->select_position_y + y > 2 || info->select_position_y + y < 0)
-        return (printf ("those are the limits, you cant skip pass the boarders !\n"), 1);
+        return (1);
     info->square[info->select_position_y][info->select_position_x].here = ' ';
     info->select_position_x += x;
     info->select_position_y += y;
     info->square[info->select_position_y][info->select_position_x].here = '*';
     return (0);
 }
+
+// int winning_algo(t_info *info)
+// {
+//     int x = info->select_position_x;
+//     int y= info->select_position_y;
+//     if (x + 1 < 3) // check if he is out of the square
+//     {
+//         // 79  O || 88  X
+//         if (info->square[y][x+1].data == '')
+        
+//     }
+// }
+
+int hit_enter(t_info *info)
+{
+    if (info->square[info->select_position_y][info->select_position_x].data != ' ')
+        return (DIDNT_HIT_3);
+    if (info->turn == 'X')
+    {
+        info->square[info->select_position_y][info->select_position_x].data = 'X';
+        info->turn = 'X' + 'O' - 'X';
+        info->reached_3_hits ++;
+    }
+    else
+    {
+        info->square[info->select_position_y][info->select_position_x].data = 'O';
+        info->turn = 'X' + 'O' - 'O';
+    }
+    // if (info->reached_3_hits >= 3)
+    // winning_algo (info);
+    return (0);
+}
+
 int main() {
     system ("clear");
     t_info info;
     init_info(&info);
     char c;
     
-    printf("Press any key (arrow keys or q to quit):\n\n");
+    printf("Press any key (arrow keys or q to quit):\n\n\n");
     display(&info);    
     while (1) {
         printf ("position x: %d\n", info.select_position_x);
         printf ("position y: %d\n", info.select_position_y);
-        
         c = getch();
         system ("clear");
-        if (c == 27) { // Escape character
-            char second = getch(); // should be '['
-            char third = getch();  // actual arrow code
-            
+        if (c == 27)
+        {
+            char second = getch();
+            char third = getch();
+
             if (second == '[') {
                 if (third == 'A')
                 {
-                    printf("Up arrow pressed\n");
+                    printf("Up arrow pressed\n\n\n");
                     move_select(&info, 0, -1);
                 }
                 else if (third == 'B')
                 {
                     move_select(&info, 0, 1);
-                    printf("Down arrow pressed\n");
+                    printf("Down arrow pressed\n\n\n");
                 }
                 else if (third == 'C')
                 {
-                    printf("Right arrow pressed\n");
+                    printf("Right arrow pressed\n\n\n");
                     move_select(&info, 1, 0);
                 }
                 else if (third == 'D')
                 {
-                    printf("Left arrow pressed\n");
+                    printf("Left arrow pressed\n\n\n");
                     move_select(&info, -1, 0);
                 }
                 else
                 {
-                    printf("Unknown escape sequence\n");
+                    printf("Unknown escape sequence\n\n\n\n");
                 }
             }
-        } else {
-            printf("You pressed: %c\n", c);
-            printf("ASCII code : %d\n", c);
-            if (c == 'q') break;
+            else
+            {
+                printf ("exit in else!\n");
+                exit(0);
+            }
+        }
+        else if (c == '\n')
+        {
+            hit_enter(&info);
         }
         display(&info);
-
     }
 
     return 0;
